677a0df3f1e93cecb32a8c93dae335e6469a1a51
MohamedElseikh <melshei1@msudenver.edu>
2021-02-15 23:18:46 -0700

    first test

---
+++ spec/support/app.js
@@ -0,0 +1,17 @@
+
+var signatureList = [];
+function addSignature(string){
+    if(string !== null){
+        for(var i = 0 ; i < signatureList.length; i++){
+            if(signatureList[i] == string){
+                return string
+            }
+            signatureList.push(string);
+        }
+    }
+    return string;
+}
+
+module.exports = {
+    addSignature : addSignature,
+}
