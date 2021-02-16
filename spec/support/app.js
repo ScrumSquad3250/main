c6fadb6d778e51d22f456b8c5f95f0e6c9e778bb
MohamedElseikh <melshei1@msudenver.edu>
2021-02-15 23:52:19 -0700

    remove signature

--- spec/support/app.js
+++ spec/support/app.js
@@ -7,8 +7,8 @@ var signatureList = [];
 function addSignature(string){
     if(string !== null){
         for(var i = 0 ; i < signatureList.length; i++){
-            if(signatureList[i] == string){
-                return string
+            if (signatureList[i] == string){
+                return string;
             }
             signatureList.push(string);
         }
@@ -35,6 +35,25 @@ function toLowerCase(string){
     return string.toLowerCase(); 
 
 } 
+
+function removeSignature(string){
+    var remove  = null;
+    if (string !== null) {
+
+        for(var i = 0 ; i < signatureList.length ; i++){
+
+            if(string == signatureList [i]){
+                remove = signatureList[i];
+                return string;
+            }
+            return false;
+
+        }
+
+    }
+    return string;
+}
+
 module.exports = {
     addSignature : addSignature,
     toUpperCase: toUpperCase,   
